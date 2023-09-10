d5c64cc324b10dabb4d75a6e0bb3b3b6ab9ac4b8
2021BTech <daffydanni@gmail.com>
2023-09-10 13:57:12 +0100

    header section updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <h1> Becon Daniel</h1>
+      <h1> Becon Daniel Oshorenoya</h1>
       <nav>
         <ul>
           <li>
