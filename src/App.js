1cffd73b49f2af11672e8b24dc5b8828629a1144
2021BTech <daffydanni@gmail.com>
2023-09-10 14:06:16 +0100

    about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Header/Header";
+import About from "./Components/About/About";
 
 function App() {
   return (
     <div>
       <Header />
+      <About />
     </div>
   );
 }
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="section">
+      <h2> About Me</h2>
+      <p>
+        Experienced Software Developer, Web Developer with a demonstrated
+        history of working in the information technology and services industry.
+        I am Skilled in Communication, Customer Relationship Management (CRM),
+        Management, Microsoft Excel, and Engineering. Strong engineering
+        professional with a Bachelor of Engineering - BE focused on
+        metallurgical/material engineering from Nnamdi Azikiwe University.
+      </p>
+    </div>
+  );
+};
+
+export default About;
